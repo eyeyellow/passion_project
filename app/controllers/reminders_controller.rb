dfae97256b09de74cc259ad89761acf73eff02c8
Frank Aiello <frankvaiello@gmail.com>
2016-09-04 17:33:33 -0700

    adds visibility toggle for reminder display

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -6,9 +6,15 @@ post '/reminders' do
     reminder_freq: params[:reminder_freq],
     entry_date: params[:entry_date]
     )
+  @entry_type_name = params[:entry_type_name]
   p @reminder.entry_date
   p @reminder.next_datetime_to_water
   p @reminder.next_date_to_water
   p @reminder.next_weekday_to_water
-  redirect '/entries'
+  if request.xhr?
+    content_type :html
+    erb :'/entries/_reminder', locals: {reminder: @reminder, entry_type_name: @entry_type_name}, layout: false
+  else
+    erb :'/entries/reminder'
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
   displayEntry();
   hideEntry();
+  displayReminder();
+  hideReminder();
 });
 
 var displayEntry = function() {
@@ -23,5 +25,27 @@ var displayEntry = function() {
 var hideEntry = function() {
   $('.entries-container').on('click', '.hide-button', function(event) {
     $(".show-entry").hide();
+  })
+}
+
+var displayReminder = function() {
+  $('.entries-container').on('submit', '.reminder-button', function(event) {
+    event.preventDefault();
+    reminderData = $(this).serialize();
+    $.ajax({
+      method: 'post',
+      url: '/reminders',
+      data: reminderData
+    })
+    .done(function(response) {
+      // console.log(response);
+      $(".entries-container").append(response);
+    })
+  })
+}
+
+var hideReminder = function() {
+  $('.entries-container').on('click', '.hide-button', function(event) {
+    $(".show-reminder").hide();
   })
 }
